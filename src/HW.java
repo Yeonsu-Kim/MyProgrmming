174d2de509401c87f1f7ffa62ecb2f1d6d9d0852
82109 <82109@DESKTOP-9VOKUGM>
2019-12-09 19:39:13 +0900

    first

---
+++ src/HW.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+class check{
+	private int temp, loop, count =0;
+	Scanner scan = new Scanner(System.in); 
+	
+	void inputLoop() {
+		this.loop = scan.nextInt();
+	}// 루프 횟수를 입력받음
+	
+	void checkWord() {
+		while(loop>0) {
+			char inputWord[]= scan.next().toCharArray();// 단어를 입력받아 배열에 저장
+			
+			for (int i = 0;i<inputWord.length-1;i++) {
+				if (inputWord[i] == inputWord[i+1])
+					inputWord[i] = ' ';
+			}// 모여있는 같은 알파벳 중 하나만 남기고 모두 공백으로 바꿈
+			
+			temp = 0;
+			
+			for (int i = 0;i<inputWord.length;i++) {
+				for (int j = i+1; j<inputWord.length;j++) {
+					if (inputWord[i] != ' ' && inputWord[i] == inputWord[j])
+						temp++;
+				}
+			}// 남은 단어 중 같은 종류의 알파벳이 있는지 체크
+			
+			if (temp == 0)
+				count++;
+			
+			loop--;
+		}
+		
+		System.out.println(count);
+	}
+}
+
+public class HW {
+	public static void main(String[] args) {
+		check c = new check();
+		c.inputLoop();
+		c.checkWord();
+	}
+}
